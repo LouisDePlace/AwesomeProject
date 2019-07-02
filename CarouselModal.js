0e2836cc08d0581c84d9e64a9260210332aa0e76
Shaabi <shaabi.mohammed@made.com>
2019-07-02 11:54:17 +0100

    got it working!

--- CarouselModal.js
+++ CarouselModal.js
@@ -1,22 +1,28 @@
 import React, { Component } from "react";
-import { View, Modal } from "react-native";
+import { SafeAreaView, Modal, Text, TouchableOpacity } from "react-native";
 import PdpImages from "./PdpImages";
 import ImageViewer from "react-native-image-zoom-viewer";
 
 class CarouselModal extends Component {
   render() {
     return (
-      <View>
-        <Modal visible={true} transparent={true}>
+      <Modal visible={true} transparent={true}>
+        <SafeAreaView style={{ flex: 1 }}>
           <ImageViewer
             imageUrls={PdpImages}
             onSwipeDown={this.props.swipeDown}
             enableSwipeDown
             index={this.props.index}
             backgroundColor={"white"}
+            renderHeader={() => (
+              <TouchableOpacity>
+                <Text>Close</Text>
+              </TouchableOpacity>
+            )}
+            renderIndicator={() => null}
           />
-        </Modal>
-      </View>
+        </SafeAreaView>
+      </Modal>
     );
   }
 }
